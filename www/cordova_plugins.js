324f05fa76cc0efb6e19d3443a6bbfa55ab7f710
dotopstory <jinbengb@gmail.com>
2015-02-14 12:26:49 +0900

    feel so good

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -6,11 +6,20 @@ module.exports = [
         "clobbers": [
             "window.plugins.AdMob"
         ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.splashscreen/www/splashscreen.js",
+        "id": "org.apache.cordova.splashscreen.SplashScreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
+    "org.apache.cordova.splashscreen": "0.3.5",
+    "com.rjfun.cordova.extension": "1.0.6",
     "com.rjfun.cordova.plugin.admob": "2.1.7",
     "com.google.playservices": "19.0.0",
     "com.google.admobsdk": "6.12.2"
